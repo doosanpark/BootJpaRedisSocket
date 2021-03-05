f3b7ee394e6dd3095b9b1123d4395b55b84d6e0f
thriller122\Administrator <erid3232@naver.com>
2021-03-05 18:27:15 +0900

    start

---
+++ src/main/java/com/example/demo/BootJpaRedisSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaRedisSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaRedisSocketApplication.class, args);
+	}
+
+}
