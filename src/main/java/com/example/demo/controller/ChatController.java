eda6d2d99af03bc8614fe1957472d88310de8693
humble_neh <erid3232@naver.com>
2021-04-13 10:08:50 +0900

    챗 컨트롤러 먼가 수정됨

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
-public class ChatController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1")
+public class ChatController {
+	
 }
